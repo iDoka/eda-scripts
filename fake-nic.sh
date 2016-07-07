b883e173477aff626dab49c83c8df15ddff317d1
Dmitry Murzinov <kakstattakim@gmail.com>
2016-07-07 17:40:15 +0300

    [fake-nic.sh] initial release of fake-nic script

---
+++ fake-nic.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+#######################################################################################
+##  Project    : Collect of various scripts for working with EDA-tools (ASIC/FPGA/etc)
+##  Designer   : Dmitry Murzinov (kakstattakim@gmail.com)
+##  Link       : https://github.com/iDoka/eda-scripts
+##  Module     : (Additional) NIC-adaptor creator
+##  Description: create virtual NIC with manually specifyed HostID 
+##               (of course, pick any random MAC)
+##  Usage      : refer to fake-nic.adoc for usage help
+##  Revision   : $Rev
+##  Date       : $LastChangedDate$
+##  License    : MIT
+#######################################################################################
+
+OS_VERSION=`uname -r | sed 's/[0-9.\-]*el\([0-9]*\)[a-zA-Z0-9_.]*/\1/g'`
+
+MAC="E4:7D:DE:AD:BE:EF"
+DEV="eth1"
+
+case $OS_VERSION in
+6*) # RHEL/CentOS 6
+  yum install -y tunctl
+  tunctl -p -u root -t ${DEV}
+  ifconfig ${DEV} hw ether ${MAC}
+  ifconfig ${DEV} up
+;;
+7*) # RHEL/CentOS 7            
+  yum install -y bridge-utils
+  ip tuntap add ${DEV} mode tap
+  ifconfig ${DEV} hw ether ${MAC}
+  ifconfig ${DEV} up
+;;    
+*) # unsupported OS
+  echo "Your OS is unsupported"
+;;
+esac
+  
