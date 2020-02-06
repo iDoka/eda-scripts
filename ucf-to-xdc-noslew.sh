0c62fa46bd640e1651021fc52fc0e124f7fed63d
Dmitry Murzinov <kakstattakim@gmail.com>
2020-02-06 12:55:41 +0300

    fixed #2

---
+++ ucf-to-xdc-noslew.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+#######################################################################################
+##  Project    : Collect of various scripts for working with EDA-tools (ASIC/FPGA/etc)
+##  Designer   : Dmitry Murzinov (kakstattakim@gmail.com)
+##  Link       : https://github.com/iDoka/eda-scripts
+##  Module     : sdc -> xdc converter
+##  Description: for Xilinx tools: that convert ISE-style to Vivado-style constraints
+##  Usage      : refer to ucf-to-xdc.adoc for usage help
+##  Revision   : $Rev
+##  Date       : $LastChangedDate$
+##  License    : MIT
+#######################################################################################
+
+
+FILE=`basename $1 | cut -d. -f1`
+# echo "${FILE}"
+
+##### obsolete syntax (saved for references):
+#sed '/^$/d;s/[\"=|;]//g;s/[\t]*//g;s/\ \{1,\}/\ /g;s/NET[ ]*\(.*\)[ ]*LOC[ ]*\([A-Z0-9]*\)[ ]*IOSTANDARD[ ]*\([_A-Z0-9]*\)[ ]*VCCAUX_IO[ ]*\([A-Z]*\)[ ]*SLEW[ ]*\([A-Z]*\)/set_property PACKAGE_PIN \2 \[get_ports \1\]\nset_property IOSTANDARD  \3 \[get_ports \1\]\nset_property VCCAUX_IO   \4 \[get_ports \1\]\nset_property SLEW        \5 \[get_ports \1\]\n\n/g' \
+#  $FILE.ucf > $FILE.xdc
+
+
+sed '/^$/d;s/[\"=|;]//g;s/[\t]*//g;s/\ \{1,\}/\ /g;s/NET[ ]*\(.*\)[ ]*LOC[ ]*\([A-Z0-9]*\)[ ]*IOSTANDARD[ ]*\([_A-Z0-9]*\)[ ]*VCCAUX_IO[ ]*\([A-Z]*\)[ ]/set_property -dict \{PACKAGE_PIN \2 \tIOSTANDARD \3 \tVCCAUX_IO \4\} \[get_ports \1\];/g' \
+  $FILE.ucf > $FILE.xdc
+  
